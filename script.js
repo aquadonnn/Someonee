160c30a458140ce441aef1175515cd0b6899a104
aquadonnn <nurdinrahmadan68@gmail.com>
2024-06-13 16:23:17 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const wrapper = document.querySelector(".wrapper");
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+
+yesBtn.addEventListener("click", () => {
+  question.innerHTML = "I love you too! 😘";
+  gif.src =
+    "https://media3.giphy.com/media/S9ceVioSdQGGRmcVkm/200w.gif?cid=82a1493bqzdehv5xnr6e0w9oca6m8er9nrwl8uyvtejkrebr&ep=v1_gifs_related&rid=200w.gif&ct=s";
+});
+
+noBtn.addEventListener("mouseover", () => {
+  const noBtnRect = noBtn.getBoundingClientRect();
+  const maxX = window.innerWidth - noBtnRect.width;
+  const maxY = window.innerHeight - noBtnRect.height;
+
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
+
+  noBtn.style.left = randomX + "px";
+  noBtn.style.top = randomY + "px";
+});
